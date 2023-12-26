85f3fdc7d44753311d95c63d8a2351604dd6528a
Daniel Arango Vieira <zendrian@gmail.com>
2023-12-26 13:15:15 -0500

    Add swipe on movile

--- src/modules/card/components/carousel/Carousel.jsx
+++ src/modules/card/components/carousel/Carousel.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import blackCap from "../../../../assets/black.png";
 import blueCap from "../../../../assets/blue.png";
 import redCap from "../../../../assets/red.png";
@@ -14,6 +14,42 @@ import {
 
 const Carousel = () => {
   const { capColor, setCapColor } = useContext(CapColorContext);
+
+  const touchStart = useRef(null);
+  const touchEnd = useRef(null);
+  const minSwipeDistance = 50;
+
+  const onTouchStart = (e) => {
+    touchEnd.current = null;
+    touchStart.current = e.targetTouches[0].clientX;
+  };
+
+  const onTouchMove = (e) => {
+    touchEnd.current = e.targetTouches[0].clientX;
+  };
+
+  const onTouchEnd = () => {
+    if (!touchStart.current || !touchEnd.current) return;
+    const distance = touchStart.current - touchEnd.current;
+    const isLeftSwipe = distance > minSwipeDistance;
+    const isRightSwipe = distance < -minSwipeDistance;
+    if (isLeftSwipe || isRightSwipe) {
+      changeCarouselImg(isLeftSwipe ? "left" : "right");
+    }
+  };
+
+  const changeCarouselImg = (dir) => {
+    const currentInx = CAP_COLOR_LIST.indexOf(capColor);
+    const newIndex = dir === "left" ? currentInx + 1 : currentInx - 1;
+
+    if (newIndex > CAP_COLOR_LIST.length - 1) {
+      setCapColor(CAP_COLOR_LIST[0]);
+    } else if (newIndex < 0) {
+      setCapColor(CAP_COLOR_LIST[CAP_COLOR_LIST.length - 1]);
+    } else {
+      setCapColor(CAP_COLOR_LIST[newIndex]);
+    }
+  };
 
   const getColorSource = () => {
     switch (capColor) {
@@ -31,7 +67,14 @@ const Carousel = () => {
   return (
     <div className="carousel">
       <div className="carousel__img-container">
-        <img className="carousel__img" src={getColorSource()} alt="Black cap" />
+        <img
+          onTouchStart={onTouchStart}
+          onTouchMove={onTouchMove}
+          onTouchEnd={onTouchEnd}
+          className="carousel__img"
+          src={getColorSource()}
+          alt="Black cap"
+        />
       </div>
       <div className="carousel__nav-container">
         {CAP_COLOR_LIST.map((col, idx) => (
